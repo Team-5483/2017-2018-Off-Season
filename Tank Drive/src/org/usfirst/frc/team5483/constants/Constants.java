7ee9b38eae0ae6899a744cd3b4acd2fd5695820b
TheLetterSix <lmaclean234@gmail.com>
2017-11-04 01:49:01 -0400

    some simple stuff

---
+++ Tank Drive/src/org/usfirst/frc/team5483/constants/Constants.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team5483.constants;
+
+public abstract class Constants {
+	//Motors for the shovel.
+	public static final int SHOVEL_LEFT = 5;
+	public static final int SHOVEL_RIGHT = 0;
+	
+	//Wheels
+	public static final int WHEEL_FRONT_LEFT = 4;
+	public static final int WHEEL_FRONT_RIGHT = 2;
+	public static final int WHEEL_BACK_LEFT = 3;
+	public static final int WHEEL_BACK_RIGHT = 6;
+	
+
+	
+}
